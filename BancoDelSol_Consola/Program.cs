7a81391d8bdf968b2de2eedbfb8893d919105b63
mavifa <55769030+mavifa@users.noreply.github.com>
2020-04-08 13:23:09 -0400

    menu listo

--- BancoDelSol_Consola/Program.cs
+++ BancoDelSol_Consola/Program.cs
@@ -10,11 +10,45 @@ namespace BancoDelSol_Consola
     {
         static void Main(string[] args)
         {
+            while (Menu());
+
+        }
+
+        private static bool Menu(){
+            bool continuar = true;
             Console.WriteLine("BancoDelSol");
-            Console.WriteLine("nuevas funciones pronto!");
-            Console.ReadKey();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1. Crear Persona");
+            Console.WriteLine("2. Mostrar Persona");
+            Console.WriteLine("3. Eliminar Persona");
+            Console.WriteLine("0. Salir de Programa");
+            Console.WriteLine("------------------------");
+            string opcion = Console.ReadLine().Trim();
 
+            switch(opcion){
 
+                case "1": 
+                    //ingresar();
+                    break;
+
+                case "2": 
+                    //mostrar();
+                    break;
+
+                case "3": 
+                    //eliminar();
+                    break;
+
+                case "0": 
+                    continuar = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Debe ingresar una opción válida");
+                    Console.ReadKey();
+                    break;
+            }
+            return continuar;
         }
     }
 }
